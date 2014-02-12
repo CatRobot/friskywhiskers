69a4cfff01313f04632d0dd2fd765d931ee5ae34
Your Actual Name <Your Actual Email>
2014-02-11 18:53:59 -0800

    This is the beginning. Yay. Hello to all worlds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FriskyWhiskers::Application.config.secret_key_base = 'a0f6274dfd4aa73144260055badc75f66739d90f601ede795f4093d01c6161602236d9c502f6b5da960ad0278d8b8545e280a46e0249a851daf030615b97d054'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FriskyWhiskers::Application.config.session_store :cookie_store, key: '_Frisky_Whiskers_session'
